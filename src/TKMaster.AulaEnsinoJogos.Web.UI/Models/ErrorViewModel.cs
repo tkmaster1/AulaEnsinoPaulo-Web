bba7fa0b54e4b81ab09e63d122bf58d06416ef70
Tatiane k. Oliveira <tatianeo.sistemas@gmail.com>
2021-12-07 20:30:14 -0300

    Versionando somente arquivos essencial. Projeto web de ensino com Bootstrap, Jquery, Javascript, C#.

---
+++ src/TKMaster.AulaEnsinoJogos.Web.UI/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TKMaster.AulaEnsinoJogos.Web.UI.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
